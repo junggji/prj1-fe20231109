3b47c6ac2ec49fc2fe15e69d397a502b7ea624a2
junggji <calrinar@gmail.com>
2023-11-09 11:12:16 +0900

    package.json에 proxy: https://localhost:8080 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { BoardWrite } from "./page/BoardWrite";
+import { BoardList } from "./page/BoardList";
+import { HomeLayout } from "./layout/HomeLayout";
+
+// homelayout -> 전체 틀 (navigation bar)
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout />}>
+      <Route index element={<BoardList />} />
+      <Route path="write" element={<BoardWrite />} />
+    </Route>,
+  ),
+);
 
 function App(props) {
-  return (
-    <div></div>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
---
+++ src/layout/HomeLayout.js
@@ -0,0 +1,14 @@
+// 경로설정
+import { Box } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import React from "react";
+import { NavBar } from "../component/NavBar";
+
+export function HomeLayout() {
+  return (
+    <Box>
+      <NavBar />
+      <Outlet />
+    </Box>
+  );
+}
