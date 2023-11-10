31c89ffba2a1f8fe138c4c500755e67e2bfcfa2b
junggji <calrinar@gmail.com>
2023-11-10 11:50:55 +0900

    회원가입 폼 작성

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import { BoardWrite } from "./page/BoardWrite";
-import { BoardList } from "./page/BoardList";
+import { BoardWrite } from "./page/board/BoardWrite";
+import { BoardList } from "./page/board/BoardList";
 import { HomeLayout } from "./layout/HomeLayout";
-import { BoardView } from "./page/BoardView";
-import { BoardEdit } from "./page/BoardEdit";
+import { BoardView } from "./page/board/BoardView";
+import { BoardEdit } from "./page/board/BoardEdit";
+import { MemberSignup } from "./page/member/MemberSignup";
 
 // homelayout -> 전체 틀 (navigation bar)
 const routes = createBrowserRouter(
@@ -19,6 +20,7 @@ const routes = createBrowserRouter(
       <Route path="write" element={<BoardWrite />} />
       <Route path="board/:id" element={<BoardView />} />
       <Route path="edit/:id" element={<BoardEdit />} />
+      <Route path="signup" element={<MemberSignup />} />
     </Route>,
   ),
 );
