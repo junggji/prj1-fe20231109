5d4d0444c60e6848c60fc95fc6c15b0f0f617d51
junggji <calrinar@gmail.com>
2023-11-14 16:36:56 +0900

    LogInProvider에 로그인 기능 모아 넣기

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import { MemberView } from "./page/member/MemberView";
 import { MemberEdit } from "./page/member/MemberEdit";
 import { MemberLogin } from "./page/member/MemberLogin";
 import axios from "axios";
+import LoginProvider from "./component/LoginProvider";
 
 // homelayout -> 전체 틀 (navigation bar)
 const routes = createBrowserRouter(
@@ -34,44 +35,11 @@ const routes = createBrowserRouter(
   ),
 );
 
-export const LoginContext = createContext(null);
-
 function App(props) {
-  const [login, setLogin] = useState("");
-
-  useEffect(() => {
-    fetchLogin();
-  }, []);
-
-  console.log(login);
-
-  function fetchLogin() {
-    axios.get("/api/member/login").then((response) => setLogin(response.data));
-  }
-
-  function isAuthenticated() {
-    return login !== "";
-  }
-
-  // js array 의 some 메소드 --> 하나라도 일치하는지 테스트 (admin인지 확인)
-  function isAdmin() {
-    if (login.auth) {
-      // some 은 list에만 있는 메소드여서, 처음 실행시에 login이 null이면 list가 아니여서 some값이 undefined오류가 나서 넣은 코드
-      return login.auth.some((elem) => elem.name === "admin");
-    }
-    return false;
-  }
-
-  function hasAccess(userId) {
-    return login.id === userId;
-  }
-
   return (
-    <LoginContext.Provider
-      value={{ login, fetchLogin, isAuthenticated, hasAccess, isAdmin }}
-    >
+    <LoginProvider>
       <RouterProvider router={routes} />
-    </LoginContext.Provider>
+    </LoginProvider>
   );
 }
 
