ab0c701345c3ed484f2740e8600911b85e585847
junggji <calrinar@gmail.com>
2023-11-14 15:57:13 +0900

    admin 기능

--- src/App.js
+++ src/App.js
@@ -53,15 +53,24 @@ function App(props) {
     return login !== "";
   }
 
+  // js array 의 some 메소드 --> 하나라도 일치하는지 테스트 (admin인지 확인)
+  function isAdmin() {
+    if (login.auth) {
+      // some 은 list에만 있는 메소드여서, 처음 실행시에 login이 null이면 list가 아니여서 some값이 undefined오류가 나서 넣은 코드
+      return login.auth.some((elem) => elem.name === "admin");
+    }
+    return false;
+  }
+
   function hasAccess(userId) {
     return login.id === userId;
   }
 
   return (
     <LoginContext.Provider
-      value={{ login, fetchLogin, isAuthenticated, hasAccess }}
+      value={{ login, fetchLogin, isAuthenticated, hasAccess, isAdmin }}
     >
-      <RouterProvider router={routes} />;
+      <RouterProvider router={routes} />
     </LoginContext.Provider>
   );
 }
