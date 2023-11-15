1af3a33a07cde8396164b6f0fa60b022167ab445
junggji <calrinar@gmail.com>
2023-11-15 11:44:18 +0900

    댓글 쓰는 중 상태(isSubmitting)을 CommentContainer로 옮김

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -15,22 +15,19 @@ import {
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-function CommentForm({ boardId }) {
+function CommentForm({ boardId, isSubmitting, onSubmit }) {
   const [comment, setComment] = useState("");
 
   function handleSubmit() {
-    axios.post("/api/comment/add", {
-      // 어느 게시물에서(boardview에서 prop으로 받아옴
-      boardId,
-      // 어떤 댓글
-      comment,
-    });
+    onSubmit({ boardId, comment });
   }
 
   return (
     <Box>
       <Textarea value={comment} onChange={(e) => setComment(e.target.value)} />
-      <Button onClick={handleSubmit}>쓰기</Button>
+      <Button isDisabled={isSubmitting} onClick={handleSubmit}>
+        쓰기
+      </Button>
     </Box>
   );
 }
@@ -55,13 +52,15 @@ function CommentList({ boardId }) {
       </CardHeader>
       <CardBody>
         <Stack divider={<StackDivider />} spacing="4">
+          {/* TODO: 댓글 작성 후 re render*/}
           {commentList.map((comment) => (
-            <Box>
+            <Box key={comment.id}>
               <Flex justifyContent="space-between">
                 <Heading size="xs">{comment.memberId}</Heading>
-                <Text fontsize="xs">{comment.inserted}</Text>
+                <Text fontSize="xs">{comment.inserted}</Text>
               </Flex>
-              <Text pt="2" fontsiz="sm">
+              {/* 새로운 줄 출력 */}
+              <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
                 {comment.comment}
               </Text>
             </Box>
@@ -73,9 +72,23 @@ function CommentList({ boardId }) {
 }
 
 export function CommentContainer({ boardId }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  function handleSubmit(comment) {
+    setIsSubmitting(true);
+
+    axios
+      .post("/api/comment/add", comment)
+      .finally(() => setIsSubmitting(false));
+  }
+
   return (
     <Box>
-      <CommentForm boardId={boardId} />
+      <CommentForm
+        boardId={boardId}
+        isSubmitting={isSubmitting}
+        onSubmit={handleSubmit}
+      />
       <CommentList boardId={boardId} />
     </Box>
   );
