c6ae88ab79d1416c1789013248344954f725c3da
junggji <calrinar@gmail.com>
2023-11-16 15:59:00 +0900

    fontawesome NavBar에 사용

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -3,6 +3,17 @@ import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useContext, useEffect } from "react";
 import { LoginContext } from "./LoginProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import {
+  faArrowRightFromBracket,
+  faHouse,
+  faPen,
+  faRightToBracket,
+  faUser,
+  faUserPlus,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
 
 export function NavBar() {
   const { fetchLogin, login, isAuthenticated, isAdmin } =
@@ -40,25 +51,46 @@ export function NavBar() {
     // isAuthenticated는 로그인 여부, isAdmin은 관리자여부
     // 로그인, 관리자 여부에 따라 Navbar가 보여질 수 있게
     <Flex>
-      <Button onClick={() => navigate("/")}>home</Button>
+      <Button onClick={() => navigate("/")}>
+        <FontAwesomeIcon icon={faHouse} />
+        home
+      </Button>
       {isAuthenticated() && (
-        <Button onClick={() => navigate("/write")}>write</Button>
+        <Button onClick={() => navigate("/write")}>
+          <FontAwesomeIcon icon={faPen} />
+          write
+        </Button>
       )}
       {isAuthenticated() || (
-        <Button onClick={() => navigate("/signup")}>signup</Button>
+        <Button onClick={() => navigate("/signup")}>
+          <FontAwesomeIcon icon={faUserPlus} />
+          signup
+        </Button>
       )}
       {isAdmin() && (
-        <Button onClick={() => navigate("/member/list")}>회원목록</Button>
+        <Button onClick={() => navigate("/member/list")}>
+          <FontAwesomeIcon icon={faUsers} />
+          회원목록
+        </Button>
       )}
       {isAuthenticated() && (
         <Button onClick={() => navigate("/member?" + urlParams.toString())}>
+          <FontAwesomeIcon icon={faUser} />
           회원정보
         </Button>
       )}
       {isAuthenticated() || (
-        <Button onClick={() => navigate("/login")}>로그인</Button>
+        <Button onClick={() => navigate("/login")}>
+          <FontAwesomeIcon icon={faRightToBracket} />
+          로그인
+        </Button>
       )}
-      {isAuthenticated() && <Button onClick={handleLogout}>로그아웃</Button>}
+      {isAuthenticated() && (
+        <Button onClick={handleLogout}>
+          <FontAwesomeIcon icon={faArrowRightFromBracket} />
+          로그아웃
+        </Button>
+      )}
     </Flex>
   );
 }
